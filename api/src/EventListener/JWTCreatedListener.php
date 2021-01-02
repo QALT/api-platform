6868f795b56864de377b25aeaf6bc29d55f180c4
Lucas <xz351@hotmail.fr>
2021-01-02 15:34:21 +0100

    Add user iri to token (#20)

--- api/src/EventListener/JWTCreatedListener.php
+++ api/src/EventListener/JWTCreatedListener.php
@@ -17,11 +17,13 @@ class JWTCreatedListener
     {
         /** @var $user \AppBundle\Entity\User */
         $user = $event->getUser();
+        $id = $user->getId();
 
         // add new data
         $payload['firstname'] = $user->getFirstname();
         $payload['lastname'] = $user->getLastname();
         $payload['email'] = $user->getUsername();
+        $payload['id'] = "/api/users/$id";
 
         $event->setData($payload);
     }
