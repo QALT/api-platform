00ad6e1dfcda5a66223dcb8847f8beafefc5a481
Thomas Corio <44027916+tcorio@users.noreply.github.com>
2021-01-23 19:42:28 +0100

    Ajout normalization study (#30)

--- api/src/Entity/Study.php
+++ api/src/Entity/Study.php
@@ -9,7 +9,10 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *  normalizationContext={"groups"={"study:read"}},
+ *  denormalizationContext={"groups"={"study:write"}}
+ * )
  * @ORM\Entity(repositoryClass=StudyRepository::class)
  */
 class Study
@@ -20,30 +23,31 @@ class Study
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @Groups({"user:read"})
+     * @Groups({"user:read","study:write","study:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"user:read"})
+     * @Groups({"user:read","study:write","study:read"})
      */
     private $label;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"user:read"})
+     * @Groups({"user:read","study:write","study:read"})
      */
     private $school;
 
     /**
      * @ORM\ManyToOne(targetEntity=Degree::class, inversedBy="studies")
-     * @Groups({"user:read"})
+     * @Groups({"user:read","study:write","study:read"})
      */
     private $degree;
 
     /**
      * @ORM\ManyToOne(targetEntity=User::class, inversedBy="studies")
+     * @Groups({"study:write","study:read"})
      */
     private $userAccount;
 
